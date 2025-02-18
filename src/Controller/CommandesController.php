4f9357ca9040d5a0816557cad23bcb05906c51fa
sahar <saharmnif72@gmail.com>
2025-02-19 00:50:13 +0100

    push de la branche sahar

--- src/Controller/CommandesController.php
+++ src/Controller/CommandesController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CommandesController extends AbstractController
 {
-    #[Route('/commandes', name: 'app_commandes')]
-    public function index(): Response
-    {
-        return $this->render('commandes/index.html.twig', [
-            'controller_name' => 'CommandesController',
-        ]);
-    }
+   
+  #[Route('/commandes', name: 'liste_commandes')]
+public function listeCommandes(EntityManagerInterface $entityManager): Response
+{
+    $commandes = $entityManager->getRepository(Commande::class)->findAll();
+
+    return $this->render('commandeadmin.html.twig', [
+        'commandes' => $commandes,
+    ]);
+}
+
+
 }
